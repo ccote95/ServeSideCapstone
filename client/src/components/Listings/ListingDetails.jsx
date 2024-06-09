6610fef84030fd6f87b687feb4ccd804ea0e5bb2
Chad Cote <chadcote58@gmail.com>
2024-06-09 16:24:51 -0400

    Created a controller to get a listing by its id

--- client/src/components/Listings/ListingDetails.jsx
+++ client/src/components/Listings/ListingDetails.jsx
@@ -1,11 +1,17 @@
 import { useParams } from "react-router-dom"
+import PageContainer from "../PageContainer.jsx"
+import { Card, CardBody } from "reactstrap"
 
 export default function ListingDetails()
 {
     const {id} = useParams
     return(
-        <div>
-            hello
-        </div>
+       <PageContainer>
+            <Card>
+                <CardBody>
+                    hello
+                </CardBody>
+            </Card>
+       </PageContainer>
     )
 }
