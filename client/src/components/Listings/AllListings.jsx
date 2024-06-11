bc2bc2142461a534395374ecbc19c06b4d57014c
Chad Cote <chadcote58@gmail.com>
2024-06-11 08:50:46 -0400

    Indentation fixes

--- client/src/components/Listings/AllListings.jsx
+++ client/src/components/Listings/AllListings.jsx
@@ -17,42 +17,35 @@ export default function AllListings()
         getAll().then(setListings)
     },[])
 
-    return(
+    return (
         <div>
             <Button className="ms-2 mt-2" color="primary" onClick={() => {navigate("create")}}>
                 New Listing
             </Button>
-        <FlexContainer>
-        {listings.map((l) => (
-            <Card className="w-25 d-flex flex-column  shadow" key={l.id} style={{maxWidth: "1200px"}}>
-                <CardLink href={`listings/${l.id}`}>
-                {!l.imageBlob ? (
-                <CardImg
-                
-                  src={
-                    l.productImg
-                  }
-                />
-              ) : (
-                <CardImg src={`data:image/jpeg;base64,${l.imageBlob}`} />
-              )}
-
-                </CardLink>
-                <CardBody>
-                <CardTitle className="m-1 fw-bold">
-                    {l.title}
-                </CardTitle>
-                <CardSubtitle className="m-1">
-                    ${l.price}
-                </CardSubtitle>
-                   
-                </CardBody>
-            </Card>
-        ))}
-        </FlexContainer>
+            <FlexContainer>
+            {listings.map((l) => (
+                <Card className="w-25 d-flex flex-column  shadow" key={l.id} style={{maxWidth: "1200px"}}>
+                    <CardLink href={`listings/${l.id}`}>
+                    {!l.imageBlob ? (
+                    <CardImg
+                        src={l.productImg}
+                    />
+                ) : (
+                    <CardImg src={`data:image/jpeg;base64,${l.imageBlob}`} />
+                )}
+                    </CardLink>
+                    <CardBody>
+                        <CardTitle className="m-1 fw-bold">
+                            {l.title}
+                        </CardTitle>
+                    <CardSubtitle className="m-1">
+                        ${l.price}
+                    </CardSubtitle>
+                    
+                    </CardBody>
+                </Card>
+            ))}
+            </FlexContainer>
         </div>
     )
 }
-// Need to add more data, with prices and temp links,
-// need to add price to listing.cs and other DTOs
-// need to do styling/
