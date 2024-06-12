f1ea727a997c53304be2d529abce348551ccfea6
Chad Cote <chadcote58@gmail.com>
2024-06-12 09:50:29 -0400

    Renderd listings based off the loggedinuser id

--- client/src/managers/listingManger.js
+++ client/src/managers/listingManger.js
@@ -19,18 +19,6 @@ export const createListing = (listing) => {
         method: "POST",
         body: listing
     })
-    // .then(response => {
-    //     if (!response.ok) {
-    //         return response.text().then(text => {
-    //             console.error('Response text:', text);
-    //             throw new Error('Network response was not ok');
-    //         });
-    //     }
-    //     return response.json();
-    // }).catch((e) => {
-    //     console.error('There was a problem with the fetch operation:', e);
-    //     throw e;
-    // });
 };
 
 export const deleteListing = (id) => {
