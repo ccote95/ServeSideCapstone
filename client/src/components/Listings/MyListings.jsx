440e230d4bb53cd7d2507b1dacf8af81c1b9b2c9
Chad Cote <chadcote58@gmail.com>
2024-06-12 12:28:48 -0400

    styling

--- client/src/components/Listings/MyListings.jsx
+++ client/src/components/Listings/MyListings.jsx
@@ -2,33 +2,46 @@ import { useEffect, useState } from "react"
 import { getAllListingsById } from "../../managers/listingManger.js"
 import PageContainer from "../PageContainer.jsx"
 import FlexContainer from "../FlexContainer.jsx"
-import { Card, CardBody, CardImg, CardLink, CardSubtitle, CardTitle } from "reactstrap"
+import { Button, Card, CardBody, CardImg, CardLink, CardSubtitle, CardTitle } from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
 export default function MyListings({loggedInUser})
 {
     const [myListings, setMyListings] = useState([])
 
+    const navigate = useNavigate()
+
     useEffect(() => {
         getAllListingsById(loggedInUser.id).then(setMyListings)
     },[])
     return(
+        <>
+        <Button 
+            className="ms-2 mt-2" 
+            color="primary" 
+            onClick={() => {navigate("/listings/create")}}
+        >
+            New Listing
+        </Button>
+        <h1 className="d-flex justify-content-center">My Listings</h1>
+        
         <FlexContainer>
         {myListings?.map((l) => (
-                <Card className="w-25 d-flex flex-column  shadow" key={l.id} style={{maxWidth: "1200px",  height: "400px"}}>
+            <Card className="w-25 d-flex flex-column  shadow" key={l.id} style={{maxWidth: "1200px",  height: "400px"}}>
                     <CardLink href={`/listings/${l.id}`}>
                     {!l.imageBlob ? (
-                    <CardImg
+                        <CardImg
                         className="mt-1"
                         src={l.productImg}
-                       style={{ height: "200px", objectFit: "contain" }}
-                    />
-                ) : (
-                    <CardImg 
-                    src={`data:image/jpeg;base64,${l.imageBlob}`}
-                    style={{ height: "200px", objectFit: "contain" }} 
-                    className="mt-1"
-                    />
-                )}
+                        style={{ height: "200px", objectFit: "contain" }}
+                        />
+                        ) : (
+                            <CardImg 
+                            src={`data:image/jpeg;base64,${l.imageBlob}`}
+                            style={{ height: "200px", objectFit: "contain" }} 
+                            className="mt-1"
+                            />
+                            )}
                     </CardLink>
                     <CardBody>
                         <CardTitle className="m-1 fs-1 fw-bold">
@@ -42,5 +55,6 @@ export default function MyListings({loggedInUser})
                 </Card>
             ))}
         </FlexContainer>
+        </>
     )
 }
