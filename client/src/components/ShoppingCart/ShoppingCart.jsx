b88454c11fa3b0f9d09abc4b62b23114478deb56
Chad Cote <chadcote58@gmail.com>
2024-06-14 16:57:26 -0400

    Added styling

--- client/src/components/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import {  getAllCartsById } from "../../managers/shoppingCartManager.js"
 import PageContainer from "../FlexContainer.jsx"
-import { Card, CardBody, CardFooter, CardHeader } from "reactstrap"
+import { Button, Card, CardBody, CardFooter, CardHeader } from "reactstrap"
 
 export default function ShoppingCart({loggedInUser})
 {
@@ -25,16 +25,23 @@ export default function ShoppingCart({loggedInUser})
     
     return(
         <PageContainer>
-          <Card>
+          <Card className="w-25">
             <CardBody>
                 {cart?.map((c) => {
-                    return <p>
+                    return <p className="fw-bold">
                         {c.listing.title} ${c.listing.price}
                     </p>
                 })}
             </CardBody>
             <CardFooter>
-                {total}
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="fw-bold">
+                        ${total}
+                    </div>
+                    <div>
+                <Button   >Place Order</Button>
+                    </div>
+                </div>
             </CardFooter>
           </Card>
         </PageContainer>
