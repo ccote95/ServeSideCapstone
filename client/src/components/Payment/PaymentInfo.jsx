3c8b61a31d4aed4e170c8b60544caab119363874
Chad Cote <chadcote58@gmail.com>
2024-06-15 14:03:28 -0400

    Added a button to add a new card on file

--- client/src/components/Payment/PaymentInfo.jsx
+++ client/src/components/Payment/PaymentInfo.jsx
@@ -1,17 +1,25 @@
-import { Table } from "reactstrap";
+import { Button, Table } from "reactstrap";
 
 export default function PaymentInfo()
 {
     return(
+    <div>
+        <div className="d-flex justify-content-between align-items-center mt-2 mb-3 me-2">
+            <h2>Payment Details</h2>
+            <Button>Add New Card</Button>
+        </div>
         <Table>
             <thead>
                 <tr>
                     <th>Name</th>
                     <th>Card Number</th>
                     <th>Card Expiration Date</th>
+                    <th></th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody></tbody>
         </Table>
+        </div>
     )
 }
