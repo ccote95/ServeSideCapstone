6b3a15a968b72032cbeb111f84c425b911d04a24
Chad Cote <chadcote58@gmail.com>
2024-06-16 11:39:34 -0400

    Added logic to render the cards details in the input fields

--- client/src/components/Payment/AddCardForm.jsx
+++ client/src/components/Payment/AddCardForm.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
-import { addNewCard } from "../../managers/paymentDetailsManager.js";
+import { addNewCard, getByCardsId } from "../../managers/paymentDetailsManager.js";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function AddCardForm({loggedInUser})
@@ -12,8 +12,22 @@ export default function AddCardForm({loggedInUser})
 
     const navigate = useNavigate()
 
-     useEffect(() => {
+    const formatDate = (date) => {
+        const d = new Date(date);
+        const year = d.getFullYear();
+        const month = (d.getMonth() + 1).toString().padStart(2, '0');
+        const day = d.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
 
+     useEffect(() => {
+        if(id)
+            {
+                getByCardsId(parseInt(id)).then(card => {
+                    setCardNumber(card.creditCardNumber)
+                    setCardExpiration(card.creditCardExpiration.toString("MM,dd,yyy"))
+                })
+            }
     },[id])
 
     const handleSubmit = (e) => {
@@ -31,11 +45,18 @@ export default function AddCardForm({loggedInUser})
         <Form className="w-50 m-auto mt-4" onSubmit={handleSubmit}>
             <FormGroup>
                 <Label className="fw-bold">Card Number</Label>
-                <Input onChange={(e) => {setCardNumber(e.target.value)}}/>
+                <Input 
+                    value={cardNumber} 
+                    onChange={(e) => {setCardNumber(e.target.value)}}
+                />
             </FormGroup>
             <FormGroup>
                 <Label className="fw-bold">Card Expiration Date</Label>
-                <Input type="date" onChange={(e) => {setCardExpiration(e.target.value)}}/>
+                <Input 
+                    value={formatDate(cardExpiration)} 
+                    type="date" 
+                    onChange={(e) => {setCardExpiration(e.target.value)}}
+                />
             </FormGroup>
             <Button color="primary" type="submit" style={{float: "right"}}>Save</Button>
         </Form>
